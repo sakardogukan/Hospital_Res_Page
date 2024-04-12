f42f9dd417feafadaf7264db8051c578358d95dd
DGKN <sakardogukan@gmail.com>
2024-04-12 17:49:05 +0300

    project files updated

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,4 +1,8 @@
 import Container from "react-bootstrap/Container"
+import { doctorData as drData } from "../helper/data"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import AddModal from "./AddModal"
 
 const Doctors = () => {
   return (
@@ -6,6 +10,22 @@ const Doctors = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row>
+        {drData.map((item) => (
+          // {drData.map(({id, img, dep, name}) => (
+          // xs={12/2=6} -> en küçük ekranlarda sadece 2 adet dr görüneceği için 6
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <img
+              src={item.img}
+              alt={item.name}
+              className="img-thumbnail doctor-img"
+            />
+            <h5>{item.name}</h5>
+            <h6>{item.dep}</h6>
+          </Col>
+        ))}
+      </Row>
+      <AddModal />
     </Container>
   )
 }
